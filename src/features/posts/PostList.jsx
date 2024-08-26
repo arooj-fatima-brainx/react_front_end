b7d9df43a547fe52dc745f4dc7462da3b05aaa5c
Arooj Fatima <arooj.fatima@brainxtech.com>
2024-08-26 15:42:30 +0500

    react app started

---
+++ src/features/posts/PostList.jsx
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from "react";
+import {API_URL} from "../../constants.js";
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadPosts() {
+      try {
+        const response = await fetch(API_URL, {
+          headers: {
+            'Accept': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const json = await response.json();
+          setPosts(json);
+        } else {
+          throw response;
+        }
+      } catch (e) {
+        setError('An error occurred');
+        console.log('Error', e);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadPosts();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id} className="post-container">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PostList;
